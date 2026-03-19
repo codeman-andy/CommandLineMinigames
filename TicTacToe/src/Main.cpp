a80799b2e91a391643dd17a7f097c02430fded01
Hawke <Howdytv@hotmail.com>
2026-03-19 00:22:14 +0000

    GetMove() calls isValid() to validate the user input Move will always be checked if it is possible before calling MakeMove() Began implementation of Printable-interface and Player-class

--- TicTacToe/src/Main.cpp
+++ TicTacToe/src/Main.cpp
@@ -4,6 +4,7 @@
 #define WINNER_FOUND 0
 
 #define INVALID_COORDINATE 0
+#define INVALID_MOVE 0
 
 #define EMPTY 0
 #define FULL 3
@@ -14,6 +15,25 @@
 #define CheckForDiagRight 3
 
 
+static void Log(const char* message) {
+	std::cout << message << std::endl;
+}
+
+class Printable {
+	virtual void PrintToConsole() = 0;
+};
+
+class Player : Printable {
+	const char* m_name;
+public:
+	Player(const char* name) {
+		m_name = name;
+	}
+
+	void PrintToConsole() {
+		Log(m_name);
+	}
+};
 
 unsigned char board[3][3] = { 0 };
 
@@ -27,10 +47,6 @@ enum letter : unsigned char { UNOCCUPIED, X, O };
 letter current_letter = X;
 
 unsigned char GAME_STATE = RUNNING;
-
-static void Log(const char* message) {
-	std::cout << message << std::endl;
-}
 
 static int CheckForWinner(short int index, unsigned char CheckFor) {
 	if (CheckFor == CheckForRow) {
@@ -80,45 +96,53 @@ static int UpdateBoard(short int x, short int y) {
 	return RUNNING;
 }
 
-static int MarkOnBoard(short int x, short int y) {
-	if (board[y][x] != UNOCCUPIED) return INVALID_COORDINATE;
+static void MarkOnBoard(short int x, short int y) {
 	board[y][x] = current_letter;
-	return RUNNING;
 }
 
-static int MakeMove(short int X, short int Y) {
-
-	if (!MarkOnBoard(X, Y)) return INVALID_COORDINATE;
+static void MakeMove(short int X, short int Y) {
+	MarkOnBoard(X, Y);
 
 	if (UpdateBoard(X, Y) == WINNER_FOUND) GAME_STATE = WINNER_FOUND;
 
 	else UpdateLetter();
+}
 
-	return RUNNING;
+static int isPossible(short int X, short int Y) {
+	if (board[Y][X] == UNOCCUPIED) return RUNNING;
+
+	else Log("The coordinate you tried to mark is already occupied. Please, choose another.");
+	return INVALID_MOVE;
+}
+
+static int isValid(short int coord) {
+	if (coord % 1 == 0 && coord >= 0 && coord <= 2) return RUNNING;
+
+	else Log("Your last coordinate was invalid. Please, type your coordinates again.");
+	return INVALID_COORDINATE;
 }
 
 static int GetMove(short int& X, short int& Y) {
-	Log("Where do you wish to place? (vertical)");
+	Log("Where do you wish to place? (vertically)");
 	std::cin >> Y;
-	if (Y < 0 || Y > 2 || Y % 1 != 0) return INVALID_COORDINATE;
+	if (!isValid(Y)) return INVALID_COORDINATE;
 
-	Log("Where do you wish to place? (horizontal)");
+	Log("Where do you wish to place? (horizontally)");
 	std::cin >> X;
-	if (X < 0 || X > 2 || X % 1 != 0) return INVALID_COORDINATE;
+	if (!isValid(X)) return INVALID_COORDINATE;
 
 	return RUNNING;
 }
 
-static void TakeTurn() {
+static int TakeTurn() {
 	short int X = 0;
 	short int Y = 0;
 
-	if (!GetMove(X, Y)) {
-		Log("Your last coordinate was invalid. Please, type your coordinates again.");
-		return;
-	}
+	if (!GetMove(X, Y)) return INVALID_COORDINATE;
 
-	if (!MakeMove(X, Y)) Log("The coordinate you tried to mark is already occupied. Please, choose another.");
+	if (!isPossible(X, Y)) return INVALID_MOVE;
+
+	MakeMove(X, Y);
 }
 
 int main() {
